646bc860afd5cb54b808ab8304a915d7e27709a8
Yana Khuchuieva <yak@peytz.dk>
2021-07-19 13:54:32 +0300

    render only the markers for selected map position

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Location } from '../types';
 import { Marker } from './Marker';
@@ -16,6 +16,34 @@ const defaultProps = {
 };
 
 export const GoogleMap: React.FC<GoogleMapsProps> = ({ locations }) => {
+  const [bounds, setBounds] = useState<any>(null);
+
+  const refreshBounds = useCallback(
+    (map: any) => {
+      setBounds(map.getBounds());
+    },
+    [setBounds]
+  );
+
+  const markers = useMemo(() => {
+    if (!bounds) {
+      return [];
+    }
+
+    return locations
+      .filter((l) => bounds.contains({ lat: l.LATITUDE, lng: l.LONGITUDE }))
+      .map((el) => {
+        return (
+          <Marker
+            text={`${el.ID}`}
+            lat={el.LATITUDE}
+            lng={el.LONGITUDE}
+            key={el.ID}
+          />
+        );
+      });
+  }, [locations, bounds]);
+
   return (
     <div className="w-1/2 h-screen">
       <GoogleMapReact
@@ -24,12 +52,10 @@ export const GoogleMap: React.FC<GoogleMapsProps> = ({ locations }) => {
         }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
+        onDragEnd={refreshBounds}
+        onGoogleApiLoaded={({ map }) => refreshBounds(map)}
       >
-        {locations.map((el) => {
-          return (
-            <Marker text="h" lat={el.LATITUDE} lng={el.LONGITUDE} key={el.ID} />
-          );
-        })}
+        {markers}
       </GoogleMapReact>
     </div>
   );
--- src/components/Marker.tsx
+++ src/components/Marker.tsx
@@ -1,11 +1,17 @@
+import { memo } from 'react';
+
 type Props = {
   text: string;
   lat: number;
   lng: number;
 };
 
-export const Marker: React.FC<Props> = ({ text }) => (
-  <div className="absolute top-2/4 left-2/4 w-5 h-5 bg-red-300 border-2 border-red-700 rounded-full transform z-50">
-    {text}
-  </div>
-);
+const MarkerRaw: React.FC<Props> = ({ text }) => {
+  return (
+    <div className="absolute top-2/4 left-2/4 w-5 h-5 bg-red-300 border-2 border-red-700 rounded-full transform z-50">
+      {text}
+    </div>
+  );
+};
+
+export const Marker = memo(MarkerRaw);
